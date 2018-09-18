bd1077cbb9a18745d74a19a1b149bc089d7f0b18
Zhang Dongliang <zhangdongliang@yunionyun.com>
2018-09-18 16:44:17 +0800

    typo

--- cmd/climc/shell/updates.go
+++ cmd/climc/shell/updates.go
@@ -1,38 +1,38 @@
 package shell
 
 import (
-        "yunion.io/x/jsonutils"
-        "yunion.io/x/onecloud/pkg/mcclient"
-        "yunion.io/x/onecloud/pkg/mcclient/modules"
-        "yunion.io/x/onecloud/pkg/mcclient/options"
+	"yunion.io/x/jsonutils"
+	"yunion.io/x/onecloud/pkg/mcclient"
+	"yunion.io/x/onecloud/pkg/mcclient/modules"
+	"yunion.io/x/onecloud/pkg/mcclient/options"
 )
 
 func init() {
-        type UpdateListOptions struct {
-                options.BaseListOptions
-                Region string `help:"cloud region ID or Name"`
-        }
-        R(&UquipdateListOptions{}, "update-list", "List updates", func(s *mcclient.ClientSession, args *UpdateListOptions) error {
-                var params *jsonutils.JSONDict
-                {
-                        var err error
-                        params, err = args.BaseListOptions.Params()
-                        if err != nil {
-                                return err
+	type UpdateListOptions struct {
+		options.BaseListOptions
+		Region string `help:"cloud region ID or Name"`
+	}
+	R(&UpdateListOptions{}, "update-list", "List updates", func(s *mcclient.ClientSession, args *UpdateListOptions) error {
+		var params *jsonutils.JSONDict
+		{
+			var err error
+			params, err = args.BaseListOptions.Params()
+			if err != nil {
+				return err
 
-                        }
-                }
-                var err error
-                var result *modules.ListResult
-                if len(args.Region) > 0 {
-                        result, err = modules.Updates.ListInContext(s, params, &modules.Cloudregions, args.Region)
-                } else {
-                        result, err = modules.Updates.List(s, params)
-                }
-                if err != nil {
-                        return err
-                }
-                printList(result, modules.Updates.GetColumns(s))
-                return nil
-        })
+			}
+		}
+		var err error
+		var result *modules.ListResult
+		if len(args.Region) > 0 {
+			result, err = modules.Updates.ListInContext(s, params, &modules.Cloudregions, args.Region)
+		} else {
+			result, err = modules.Updates.List(s, params)
+		}
+		if err != nil {
+			return err
+		}
+		printList(result, modules.Updates.GetColumns(s))
+		return nil
+	})
 }
